04704d9638b52dc83285ee60478fef6a18c78293
Dima9702 <rewq9702@gmail.com>
2019-05-15 14:44:21 +0300

    steve2 v2.0 first try on new structure.

--- src/com/epam/javacore2019/steve2/db/misc/DataEncryptor.java
+++
@@ -1,5 +0,0 @@
-package com.epam.javacore2019.steve2.db.misc;
-
-public interface DataEncryptor {
-    String encrypt(String text);
-}
--- src/com/epam/javacore2019/steve2/ui/UIApplication.java
+++
@@ -1,5 +0,0 @@
-package com.epam.javacore2019.steve2.ui;
-
-public class UIApplication {
-
-};
